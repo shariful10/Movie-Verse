42ffa134ab945151d70169415de4ed0abe11816b
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-22 15:09:35 +0600

    Navbar Implemented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
-import { useRouter } from "next/router";
+"use client";
+import Link from "next/link";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
 	const [input, setInput] = useState("");
@@ -11,7 +13,18 @@ const Navbar = () => {
 		router.push(`/search/${input}?page=1`);
 	};
 
-	return <div>Navbar</div>;
+	return (
+		<div className="bg-primary">
+			<div className="flex justify-between items-center py-4 px-2 md:px-10">
+				<Link
+					className="hidden md:block"
+					href={"/discover/now_playing"}
+				>
+					<h2 className="text-[30px]">Movie Verse</h2>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
