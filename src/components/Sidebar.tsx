e14d2cd305e8024057dd75fe0b310502184a23ee
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-24 00:00:05 +0600

    Get ALL gENRES Data

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,33 @@
 "use client";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "next/navigation";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "@/utils/const";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+	const params = useParams();
+	const searchParams = useSearchParams();
+	const [genres, setGenres] = useState([]);
+	const [selectedGenres, setSelectedGenres] = useState("");
 
-export default Sidebar
+	interface Igenre {
+		id: string;
+		name: string;
+	}
+
+	useEffect(() => {
+		axios
+			.get(
+				`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`
+			)
+			.then(({ data }) => {
+				setGenres(data.genres);
+				console.log(data.genres);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
+	return <div>Sidebar</div>;
+};
+
+export default Sidebar;
