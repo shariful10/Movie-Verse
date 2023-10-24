2071bbde2c94d998163a21eed30f128f0d470600
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-24 17:22:22 +0600

    Set The Movies, Current Page & Total Page On Discover Page

--- src/app/discover/[id]/page.tsx
+++ src/app/discover/[id]/page.tsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+import { API_KEY, BASE_URL } from "@/utils/const";
 
 export interface Imovie {
 	id: string;
@@ -8,11 +12,60 @@ export interface Imovie {
 }
 
 const Discover = () => {
+	const router = useRouter();
+	const params = useParams();
+	const searchParams = useSearchParams();
+
 	const [title, setTitle] = useState("");
 	const [movies, setMovies] = useState([]);
 	const [discover, setDiscover] = useState("");
 	const [totalPage, setTotalPage] = useState(1);
 	const [currentPage, setCurrentPage] = useState(1);
+
+	const mainRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		mainRef?.current?.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: "smooth",
+		});
+		const id = params.id.toString();
+		const page = searchParams.get("page");
+
+		setDiscover(id);
+
+		switch (id) {
+			case "now_playing":
+				setTitle("Now Playing Movies");
+				break;
+			case "top_rated":
+				setTitle("Top Rated Movies");
+				break;
+			case "popular":
+				setTitle("Popular Movies");
+				break;
+			case "upcoming":
+				setTitle("Upcoming Movies");
+				break;
+			default:
+				setTitle("");
+				break;
+		}
+		axios
+			.get(`${BASE_URL}/movie/${id}`, {
+				params: {
+					api_key: API_KEY,
+					page,
+				},
+			})
+			.then((res) => {
+				setMovies(res.data.results);
+				setCurrentPage(res.data.page);
+				setTotalPage(res.data.total_page);
+			})
+			.catch((err) => console.log(err));
+	}, [params.id, searchParams]);
 
 	return <div>Discover</div>;
 };
