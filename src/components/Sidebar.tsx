bb7d772382fcac53571001d99b1b956e68bccfd7
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-24 15:13:59 +0600

    Implemented The Caegory Of Discover On Siderbar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "next/navigation";
 import axios from "axios";
 import { API_KEY, BASE_URL } from "@/utils/const";
+import Link from "next/link";
 
 const Sidebar = () => {
 	const params = useParams();
 	const searchParams = useSearchParams();
 	const [genres, setGenres] = useState([]);
-	const [selectedGenres, setSelectedGenres] = useState("");
+	const [selectedGenre, setSelectedGenre] = useState("");
 
 	interface Igenre {
 		id: string;
@@ -29,16 +30,54 @@ const Sidebar = () => {
 
 	useEffect(() => {
 		if (searchParams.get("genre")) {
-			setSelectedGenres(searchParams.get("genre")?.toString()!);
+			setSelectedGenre(searchParams.get("genre")?.toString()!);
 			return;
 		}
 	}, [params.id, searchParams]);
 
-	return <div className="bg-primary px-10 max-h-[calc(100vh-77px)] pb-6 overflow-y-scroll scrollbar-thumb-[#22222A] scrollbar-track-primary hidden sm:block">
-		<div className="flex flex-col gap-4 pt-4">
-			<p className="font-semibold text-[18px]">Discover</p>
+	return (
+		<div className="bg-primary px-10 max-h-[calc(100vh-77px)] pb-6 overflow-y-scroll scrollbar-thumb-[#22222A] scrollbar-track-primary hidden sm:block">
+			<div className="flex flex-col gap-4 pt-4">
+				<p className="font-semibold text-[18px]">Discover</p>
+				<Link href={"/discover/now_playing"}>
+					<p
+						className={`text-textColor ml-4 cursor-pointer hover:text-white transition w-fit ${
+							selectedGenre === "now_playing" ? "text-white" : ""
+						}`}
+					>
+						Now Playing
+					</p>
+				</Link>
+				<Link href={"/discover/top_rated"}>
+					<p
+						className={`text-textColor ml-4 cursor-pointer hover:text-white transition w-fit ${
+							selectedGenre === "top_rated" ? "text-white" : ""
+						}`}
+					>
+						Top Rated
+					</p>
+				</Link>
+				<Link href={"/discover/popular"}>
+					<p
+						className={`text-textColor ml-4 cursor-pointer hover:text-white transition w-fit ${
+							selectedGenre === "popular" ? "text-white" : ""
+						}`}
+					>
+						Popular
+					</p>
+				</Link>
+				<Link href={"/discover/upcoming"}>
+					<p
+						className={`text-textColor ml-4 cursor-pointer hover:text-white transition w-fit ${
+							selectedGenre === "upcoming" ? "text-white" : ""
+						}`}
+					>
+						Upcoming
+					</p>
+				</Link>
+			</div>
 		</div>
-	</div>;
+	);
 };
 
 export default Sidebar;
