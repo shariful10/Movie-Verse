da1fc195e828e01def6c8e44db08ced6a779e821
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-25 00:11:45 +0600

    Animation Implemented On Card

--- src/components/CardSkeleton.tsx
+++ src/components/CardSkeleton.tsx
@@ -2,7 +2,7 @@ import { CiImageOff } from "react-icons/ci"
 
 const CardSkeleton = ({error}: {error?: boolean}) => {
   return (
-    <div className={`h-[450px] md:h-[335px] w-full grid place-items-center bg-primary ${!error && "cardSkeletion"}`}>
+    <div className={`h-[450px] md:h-[335px] w-full grid place-items-center bg-primary ${!error && "card__skeletion"}`}>
       {error && <CiImageOff size={56} />}
     </div>
   )
