ccdde9c85390b57060f6f4970fd10bedc7733417
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-25 15:40:00 +0600

    Image & Title Implemented On Details Page

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import axios from "axios";
 import dynamic from "next/dynamic";
-import { API_KEY } from "@/utils/const";
+import { API_KEY, BASE_IMG_URL } from "@/utils/const";
 import { IoMdClose } from "react-icons/io";
 import { BsPlayFill } from "react-icons/bs";
 import { useParams, useRouter } from "next/navigation";
 import React, { useEffect, useRef, useState } from "react";
+import Footer from "@/components/Footer";
+import Loading from "@/components/Loading";
 
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
@@ -46,43 +49,43 @@ export interface BelongsToCollection {
 }
 
 export interface Genre {
-   id: number;
-   name: string;
+	id: number;
+	name: string;
 }
 
 export interface productionCompany {
-   id: number;
-   name: string;
-   logo_path: string;
-   origin_country: string;
+	id: number;
+	name: string;
+	logo_path: string;
+	origin_country: string;
 }
 
 export interface productionCountry {
-   name: string;
+	name: string;
 	iso_3166_1: string;
 }
 
 export interface SpokenLanguage {
-   name: string;
+	name: string;
 	iso_639_1: string;
 	english_name: string;
 }
 
 export interface Videos {
-   results: Result[];
+	results: Result[];
 }
 
 export interface Result {
-   id: string;
-   key: string;
-   name: string;
-   site: string;
-   size: string;
-   type: string;
-   official: boolean;
-   iso_639_1: string;
-   iso_3166_1: string;
-   published_at: string;
+	id: string;
+	key: string;
+	name: string;
+	site: string;
+	size: string;
+	type: string;
+	official: boolean;
+	iso_639_1: string;
+	iso_3166_1: string;
+	published_at: string;
 }
 
 const MovieDetails = () => {
@@ -117,7 +120,38 @@ const MovieDetails = () => {
 		setTrailer(trailerURL);
 	}, [movie]);
 
-	return <div>MovieDetails</div>;
+	return (
+		<main
+			ref={mainRef}
+			className="bg-secondary p-8 max-h-[calc(100vh-77px)] min-h-[calc(100vh-77px)] overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-thumb-[#22222A] scroll-smooth relative scrollbar-track-primary"
+		>
+			{movie === null && <Loading />}
+			<div
+				onClick={router.back}
+				className="text-textColor hover:text-white absolute right-0 top-0 m-2 cursor-pointer"
+			>
+				<IoMdClose size={27} />
+			</div>
+			<div className="flex justify-center items-center pt-4 md:pt-0">
+				<div className="grid md:grid-cols-[300px,1fr] max-w-[1200px] gap-12">
+					<div>
+						<img
+							src={`${BASE_IMG_URL}${movie?.poster_path}`}
+							alt={movie?.title}
+						/>
+					</div>
+               <div className="space-y-6 md:space-y-3 text-textColor">
+                  <div className="uppercase text-[26px] md:text-[34px] font-medium pr-4 text-white">
+                     {movie?.title}
+                  </div>
+               </div>
+				</div>
+			</div>
+			<div className="pt-10">
+				<Footer />
+			</div>
+		</main>
+	);
 };
 
 export default MovieDetails;
